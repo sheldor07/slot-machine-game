7d77afb12734b2961fa9df21545ba4938c592341
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2022-12-23 15:16:12 +0530

    added footer and changes styling

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ let inpSpeed=document.getElementById("inpSpeed")
 let value1=document.getElementById('value1')
 let value2=document.getElementById('value2')
 let value3=document.getElementById('value3')
+let slot=document.getElementById('slot1')
+let slot2=document.getElementById('slot2')
+let slot3=document.getElementById('slot3')
 let resAlert=document.getElementById('resAlert')
 let btnSpin=document.getElementById('btnSpin')
 let spinCount=0;
+
 
 let values=['😃','🥰','🫡','🫠','😊','😘','🥲','🤗','😰','😩']
 function getRandomValue(){
@@ -18,11 +22,9 @@ function updateAnimation(newSpeed) {
         value1.innerText = getRandomValue()
         value2.innerText = getRandomValue()
         value3.innerText = getRandomValue()
-        console.log(spinCount)
-        console.log(newSpeed)
         if(++spinCount===10){
             clearInterval(animationId)
-            setTimeout(()=>{ checkWin(value1.innerText,value2.innerText,value3.innerText)},1000)
+            setTimeout(()=>{ checkWin(value1.innerText,value2.innerText,value3.innerText)},900)
            
         }
     }, 1000 / newSpeed)
@@ -42,7 +44,6 @@ function checkWin(value1, value2, value3){
 btnSpin.onclick = ()=>{
     document.documentElement.style.setProperty("--speed",5)
     updateAnimation(5)
-    
 
 }
 btnReset.onclick =()=>{
@@ -50,6 +51,11 @@ btnReset.onclick =()=>{
     value2.innerText = "❓"
     value3.innerText = "❓"
     spinCount=0
+    slot1.style.animation='none'
+    slot1.offsetHeight;
+    slot1.style.animation=null
+    
     document.documentElement.style.setProperty("--speed",0)
+    document.documentElement.style.setProperty("--iteration-count",30)
     resAlert.innerText="Spin it!"
 }
