92a6d8e905ad1975f9fbbcc080b2c960a4f91f15
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2022-12-23 14:52:21 +0530

    added spin and reset button functionality with alerts

--- script.js
+++ script.js
@@ -2,16 +2,54 @@ let inpSpeed=document.getElementById("inpSpeed")
 let value1=document.getElementById('value1')
 let value2=document.getElementById('value2')
 let value3=document.getElementById('value3')
-setInterval(()=>{
-    value1.textContent=getRandomValue()
-    value2.textContent=getRandomValue()
-    value3.textContent=getRandomValue()
-},inpSpeed.value*100)
+let resAlert=document.getElementById('resAlert')
+let btnSpin=document.getElementById('btnSpin')
+let spinCount=0;
+
 let values=['😃','🥰','🫡','🫠','😊','😘','🥲','🤗','😰','😩']
 function getRandomValue(){
     return values[Math.floor(Math.random()*10)]
 }
 
-inpSpeed.onchange = function(){
-    document.documentElement.style.setProperty("--speed",inpSpeed.value)
+let animationId;
+let animationNone=()=>{}
+function updateAnimation(newSpeed) {
+    animationId = setInterval(() => {
+        value1.innerText = getRandomValue()
+        value2.innerText = getRandomValue()
+        value3.innerText = getRandomValue()
+        console.log(spinCount)
+        console.log(newSpeed)
+        if(++spinCount===10){
+            clearInterval(animationId)
+            setTimeout(()=>{ checkWin(value1.innerText,value2.innerText,value3.innerText)},1000)
+           
+        }
+    }, 1000 / newSpeed)
+}
+function checkWin(value1, value2, value3){
+    if(value1===value2 && value2===value3){
+        resAlert.textContent="You win!"
+    }
+    else if(value1===value2 || value2===value3 || value1===value3)
+    {
+        resAlert.textContent="Oof! So Close"
+    }
+    else{
+        resAlert.textContent="What an L smh"
+    }
+}
+btnSpin.onclick = ()=>{
+    document.documentElement.style.setProperty("--speed",5)
+    updateAnimation(5)
+    
+
+}
+btnReset.onclick =()=>{
+    value1.innerText = "❓"
+    value2.innerText = "❓"
+    value3.innerText = "❓"
+    spinCount=0
+    document.documentElement.style.setProperty("--speed",0)
+    resAlert.innerText="Spin it!"
 }
